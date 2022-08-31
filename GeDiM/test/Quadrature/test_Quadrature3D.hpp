2c2b61048890158008c6ad59bafc3df1b3b6e16b
fabio.vicini <fabio.vicini@polito.it>
2022-08-31 14:10:51 +0200

    add: tetra quadrature

--- GeDiM/test/Quadrature/test_Quadrature3D.hpp
+++ GeDiM/test/Quadrature/test_Quadrature3D.hpp
@@ -53,6 +53,94 @@ namespace UnitTesting
       FAIL();
     }
   }
+
+  TEST(TestQuadrature3D, TestQuadrature_Gauss3D_Tetrahedron_YToN)
+  {
+    try
+    {
+      Gedim::GeometryUtilitiesConfig geometryUtilitiesConfig;
+      geometryUtilitiesConfig.Tolerance = 1.0e-12;
+      Gedim::GeometryUtilities geometryUtilities(geometryUtilitiesConfig);
+
+      unsigned int minOrder = 0;
+      unsigned int maxOrder = 15;
+
+      Eigen::VectorXi quadratureOrders = Eigen::VectorXi::LinSpaced(maxOrder + 1, minOrder, maxOrder);
+      Eigen::VectorXi orderMax = Eigen::VectorXi::LinSpaced(maxOrder + 1, minOrder, maxOrder);
+
+      for (unsigned int numOrd = 0; numOrd < orderMax.size(); numOrd++)
+      {
+        Eigen::MatrixXd points;
+        Eigen::VectorXd weights;
+
+        Gedim::Quadrature_Gauss3D_Tetrahedron::FillPointsAndWeights(quadratureOrders[numOrd],
+                                                                    points,
+                                                                    weights);
+
+        ASSERT_TRUE(geometryUtilities.Are1DValuesEqual(1.0 / 6.0, weights.sum()));
+
+        for(unsigned int ord = 0; ord <= orderMax[numOrd]; ord++)
+        {
+          Eigen::VectorXd pointsY = (points.row(1));
+          Eigen::VectorXd pointsYPow = (pointsY.array()).pow(ord);
+          double result = pointsYPow.dot(weights);
+
+          double expectedResult =  1.0/((ord+1) * (ord+2) * (ord+3));
+
+          ASSERT_TRUE(geometryUtilities.Are1DValuesEqual(expectedResult, result));
+        }
+      }
+    }
+    catch (const std::exception& exception)
+    {
+      std::cerr<< exception.what()<< std::endl;
+      FAIL();
+    }
+  }
+
+  TEST(TestQuadrature3D, TestQuadrature_Gauss3D_Tetrahedron_ZToN)
+  {
+    try
+    {
+      Gedim::GeometryUtilitiesConfig geometryUtilitiesConfig;
+      geometryUtilitiesConfig.Tolerance = 1.0e-12;
+      Gedim::GeometryUtilities geometryUtilities(geometryUtilitiesConfig);
+
+      unsigned int minOrder = 0;
+      unsigned int maxOrder = 15;
+
+      Eigen::VectorXi quadratureOrders = Eigen::VectorXi::LinSpaced(maxOrder + 1, minOrder, maxOrder);
+      Eigen::VectorXi orderMax = Eigen::VectorXi::LinSpaced(maxOrder + 1, minOrder, maxOrder);
+
+      for (unsigned int numOrd = 0; numOrd < orderMax.size(); numOrd++)
+      {
+        Eigen::MatrixXd points;
+        Eigen::VectorXd weights;
+
+        Gedim::Quadrature_Gauss3D_Tetrahedron::FillPointsAndWeights(quadratureOrders[numOrd],
+                                                                    points,
+                                                                    weights);
+
+        ASSERT_TRUE(geometryUtilities.Are1DValuesEqual(1.0 / 6.0, weights.sum()));
+
+        for(unsigned int ord = 0; ord <= orderMax[numOrd]; ord++)
+        {
+          Eigen::VectorXd pointsZ = (points.row(2));
+          Eigen::VectorXd pointsZPow = (pointsZ.array()).pow(ord);
+          double result = pointsZPow.dot(weights);
+
+          double expectedResult =  1.0/((ord+1) * (ord+2) * (ord+3));
+
+          ASSERT_TRUE(geometryUtilities.Are1DValuesEqual(expectedResult, result));
+        }
+      }
+    }
+    catch (const std::exception& exception)
+    {
+      std::cerr<< exception.what()<< std::endl;
+      FAIL();
+    }
+  }
 }
 
 #endif // __TEST_QUADRATURE3D_H
