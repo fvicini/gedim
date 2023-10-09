a190e943b58528b7f725aa4ce9e63396706c28f9
Fabio Vicini <90675116+fvicini@users.noreply.github.com>
2023-10-09 11:49:25 +0200

    Vem3 d (#95)
    * Add voro++ e fix bug in comparevalues
    
    * added voro interface
    
    * fix voro interface to gedim
    
    * fix: build with no VOro
    
    ---------
    
    Co-authored-by: Gioana Teora <gioana.teora@gmail.it>
    Co-authored-by: gioanateora <gioana.teora@polito.it>

---
+++ GeDiM/src/Mesh/VoroInterface.hpp
@@ -0,0 +1,72 @@
+#ifndef __VoroInterface_H
+#define __VoroInterface_H
+
+#include "GeometryUtilities.hpp"
+
+#include "IMeshDAO.hpp"
+using namespace std;
+
+#if ENABLE_VORO == 1
+#include "voro++.hh"
+#endif
+
+namespace Gedim
+{
+  class VoroInterface final
+  {
+    public:
+      struct Cell0D
+      {
+          const double x;
+          const double y;
+          const double z;
+
+          unsigned int id;
+          unsigned int marker;
+
+          Cell0D(const double x,
+                 const double y,
+                 const double z): x(x), y(y), z(z) {};
+      };
+
+      struct Cell2D
+      {
+          unsigned int id;
+          int marker;
+          vector<unsigned int> vertices;
+          vector<unsigned int> edges;
+      };
+
+    private:
+
+      const Gedim::GeometryUtilities& geometryUtilities;
+
+    public:
+      VoroInterface(const Gedim::GeometryUtilities& geometryUtilities);
+
+      void GenerateVoronoiTassellations(const Eigen::MatrixXd& polyhedronVertices,
+                                        const Eigen::MatrixXi& polyhedronEdges,
+                                        const std::vector<Eigen::MatrixXi>& polyhedronFaces,
+                                        const unsigned int &numPoints,
+                                        Gedim::IMeshDAO& mesh);
+
+    private:
+
+#if ENABLE_VORO == 1
+      bool InsertNewPoints(Cell0D& cell0D, list<Cell0D>& cell0Ds);
+      inline double rnd() {return double(rand())/RAND_MAX;}
+
+      void GenerateRandomPoints(const Eigen::MatrixXd &polyhedronVertices,
+                                const unsigned int &numPoints,
+                                voro::container &con);
+
+      void GenerateCartesianPoints(const Eigen::MatrixXd &polyhedronVertices,
+                                   const unsigned int &numPoints,
+                                   voro::container &con);
+#endif
+  };
+
+}
+
+#endif
+
