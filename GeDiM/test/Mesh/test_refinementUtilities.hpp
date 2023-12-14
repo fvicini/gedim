bfb1f6770e22902043a53e29fdd4388305dc87fc
fabio.vicini <fabio.vicini@polito.it>
2023-12-14 10:39:23 +0100

    add: tests

--- GeDiM/test/Mesh/test_refinementUtilities.hpp
+++ GeDiM/test/Mesh/test_refinementUtilities.hpp
@@ -179,24 +179,56 @@ namespace GedimUnitTesting
                                                          planeRotationMatrix,
                                                          planeTranslation,
                                                          meshDAO);
-    //    EXPECT_EQ(std::vector<unsigned int>({ 4 }),
-    //              result.NewCell0DsIndex);
-    //    EXPECT_EQ(2,
-    //              result.NewCell1DsIndex.size());
-    //    EXPECT_EQ(Gedim::RefinementUtilities::RefinePolygon_Result::RefinedCell1D::Types::Updated,
-    //              result.NewCell1DsIndex[0].Type);
-    //    EXPECT_EQ(std::vector<unsigned int>({ 5, 6 }),
-    //              result.NewCell1DsIndex[0].NewCell1DsIndex);
-    //    EXPECT_EQ(2,
-    //              result.NewCell1DsIndex[0].OriginalCell1DIndex);
-    //    EXPECT_EQ(4,
-    //              result.NewCell1DsIndex[0].NewCell0DIndex);
-    //    EXPECT_EQ(Gedim::RefinementUtilities::RefinePolygon_Result::RefinedCell1D::Types::New,
-    //              result.NewCell1DsIndex[1].Type);
-    //    EXPECT_EQ(std::vector<unsigned int>({ 7 }),
-    //              result.NewCell1DsIndex[1].NewCell1DsIndex);
-    //    EXPECT_EQ(std::vector<unsigned int>({ 2, 3 }),
-    //              result.NewCell2DsIndex);
+    ASSERT_EQ(std::vector<unsigned int>({ 20 }),
+              result.NewCell0DsIndex);
+    ASSERT_EQ(3,
+              result.NewCell1DsIndex.size());
+    ASSERT_EQ(Gedim::RefinementUtilities::RefinePolyhedron_Result::RefinedCell1D::Types::Updated,
+              result.NewCell1DsIndex[0].Type);
+    ASSERT_EQ(std::vector<unsigned int>({ 59, 60 }),
+              result.NewCell1DsIndex[0].NewCell1DsIndex);
+    ASSERT_EQ(2,
+              result.NewCell1DsIndex[0].OriginalCell1DIndex);
+    ASSERT_EQ(20,
+              result.NewCell1DsIndex[0].NewCell0DIndex);
+    ASSERT_EQ(2,
+              result.NewCell1DsIndex[0].OriginalCell3DEdgeIndex);
+    ASSERT_EQ(Gedim::RefinementUtilities::RefinePolyhedron_Result::RefinedCell1D::Types::New,
+              result.NewCell1DsIndex[1].Type);
+    ASSERT_EQ(std::vector<unsigned int>({ 61 }),
+              result.NewCell1DsIndex[1].NewCell1DsIndex);
+    ASSERT_EQ(Gedim::RefinementUtilities::RefinePolyhedron_Result::RefinedCell1D::Types::New,
+              result.NewCell1DsIndex[2].Type);
+    ASSERT_EQ(std::vector<unsigned int>({ 62 }),
+              result.NewCell1DsIndex[2].NewCell1DsIndex);
+    ASSERT_EQ(3,
+              result.NewCell2DsIndex.size());
+    ASSERT_EQ(Gedim::RefinementUtilities::RefinePolyhedron_Result::RefinedCell2D::Types::Updated,
+              result.NewCell2DsIndex[0].Type);
+    ASSERT_EQ(std::vector<unsigned int>({ 62, 63 }),
+              result.NewCell2DsIndex[0].NewCell2DsIndex);
+    ASSERT_EQ(2,
+              result.NewCell2DsIndex[0].OriginalCell2DIndex);
+    ASSERT_EQ(61,
+              result.NewCell2DsIndex[0].NewCell1DIndex);
+    ASSERT_EQ(2,
+              result.NewCell2DsIndex[0].OriginalCell3DFaceIndex);
+    ASSERT_EQ(Gedim::RefinementUtilities::RefinePolyhedron_Result::RefinedCell2D::Types::Updated,
+              result.NewCell2DsIndex[1].Type);
+    ASSERT_EQ(std::vector<unsigned int>({ 64, 65 }),
+              result.NewCell2DsIndex[1].NewCell2DsIndex);
+    ASSERT_EQ(3,
+              result.NewCell2DsIndex[1].OriginalCell2DIndex);
+    ASSERT_EQ(62,
+              result.NewCell2DsIndex[1].NewCell1DIndex);
+    ASSERT_EQ(3,
+              result.NewCell2DsIndex[1].OriginalCell3DFaceIndex);
+    ASSERT_EQ(Gedim::RefinementUtilities::RefinePolyhedron_Result::RefinedCell2D::Types::New,
+              result.NewCell2DsIndex[2].Type);
+    ASSERT_EQ(std::vector<unsigned int>({ 66 }),
+              result.NewCell2DsIndex[2].NewCell2DsIndex);
+//    ASSERT_EQ(std::vector<unsigned int>({ 2, 3 }),
+//              result.NewCell2DsIndex);
 
     //    for (unsigned int e = 0; e < result.NewCell1DsIndex.size(); e++)
     //    {
