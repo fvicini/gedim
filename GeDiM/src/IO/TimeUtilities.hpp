d1ab11cc0e78dc1cc6597b6e15fdd8b283812c52
Gioana Teora <gioana.teora@gmail.it>
2023-12-15 11:03:27 +0100

    promote timeutilities

---
+++ GeDiM/src/IO/TimeUtilities.hpp
@@ -0,0 +1,42 @@
+#ifndef __TimeUtilities_H
+#define __TimeUtilities_H
+
+#include <limits>
+#include <iostream>
+#include <vector>
+
+#include "chrono"
+
+namespace Gedim
+{
+
+template <class DT = std::chrono::milliseconds,
+         class ClockT = std::chrono::steady_clock>
+class Timer
+{
+    using timep_t = decltype(ClockT::now());
+
+    timep_t _start = ClockT::now();
+    timep_t _end = {};
+
+public:
+    void tick() {
+        _end = timep_t{};
+        _start = ClockT::now();
+    }
+
+    void tock() {
+        _end = ClockT::now();
+    }
+
+    template <class duration_t = DT>
+    duration_t duration() const {
+        // Use gsl_Expects if your project supports it.
+        assert(_end != timep_t{} && "Timer must toc before reading the time");
+        return std::chrono::duration_cast<duration_t>(_end - _start);
+    }
+};
+
+}
+
+#endif
